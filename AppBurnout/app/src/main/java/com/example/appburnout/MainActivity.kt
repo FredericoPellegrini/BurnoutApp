5babc19c621fb9b36e18a7f1ffe713b2b22c0b44
Frederico Pellegrini <fredericopellegrini@proton.me>
2024-09-03 10:11:05 -0300

    Primeira parte pronta

--- AppBurnout/app/src/main/java/com/example/appburnout/MainActivity.kt
+++ AppBurnout/app/src/main/java/com/example/appburnout/MainActivity.kt
@@ -1,47 +1,27 @@
 package com.example.appburnout
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.appburnout.ui.theme.AppBurnoutTheme
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            AppBurnoutTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+
+        val btnSobre: Button = findViewById(R.id.btn_sobre)
+        val btnConteudo: Button = findViewById(R.id.btn_conteudo)
+
+        btnSobre.setOnClickListener {
+            val intent = Intent(this, SobreActivity::class.java)
+            startActivity(intent)
         }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppBurnoutTheme {
-        Greeting("Android")
+        btnConteudo.setOnClickListener {
+            val intent = Intent(this, ConteudoActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ AppBurnout/app/src/main/java/com/example/appburnout/SobreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.appburnout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SobreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sobre)
+    }
+}
